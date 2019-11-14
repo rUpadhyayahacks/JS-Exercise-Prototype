33cd1f143991cb66ad103dac66d987caf297afc8
R <r@Rs-MacBook-Air.local>
2019-11-14 08:09:41 -0800

    raj

--- index.js
+++ index.js
@@ -39,9 +39,30 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+this.name = name,
+this.age = age,
+this.stomach = []
 }
+Person.prototype.eat = function(eat){
+  if(this.stomach.length < 10){
+    this.stomach.push(eat);
+    }else{
+      return this.stomach
+    }
+  }
+
+Person.prototype.poop = function(poop){
+  if(this.stomach.length = 0){
+  function empty(stomach){
+    stomach = []
+  }
+  }
+}
+  Person.prototype.toString = function(){
+    return `${this.name}, + ${this.age}`;
+  }
+
 
 /*
   TASK 2
@@ -57,10 +78,19 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
-
+Car.prototype.fill = function(gallons){
+  this.tank = this.tank + gallons;
+}
+Car.prototype.drive = function(distance){
+  this.odometer = this.odometer + distance;
+  this.tank = distance / this.milesPerGallon;
+}
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -68,18 +98,29 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
+function Baby(name, age, favoriteToy) {
+Person.call(this)
+this.name = name,
+this.age = age,
+this.favoriteToy = favoriteToy;
 
 }
+Baby.prototype = Object.create(Person.prototype)
+
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
+
+
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. When a function is in the global scope, the value of this inside of that function will be the window object.
+  2. When a function is called by a dot, the object before that dot is this.
+  3. When a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
+  4. Whenever JavaScript’s call or apply method is used, this is explicitly defined.
 */
 
 
